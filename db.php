88e862c3c64118ee17474143a9d9c41268bf3f07
Felipe <f.alfonso.go@gmail.com>
2016-06-08 00:16:32 -0300

    Update db.php

--- db.php
+++ db.php
@@ -7,30 +7,30 @@ new: KWMEEGvaIpiyIrsd
 */
 
 /* -------------- local host -----------------
-$link1 = mysql_connect ("localhost", "root", "fafarafa")
+$link1 = mysql_connect ("localhost", "root", "")
         or die ("No se puede conectar a MySQL ");
 
 mysql_select_db ("750") ;
 
 //
 
-	$link2 = mysql_connect ("localhost", "root", "fafarafa")
+	$link2 = mysql_connect ("localhost", "root", "")
         or die ("No se puede conectar a MySQL ");
 
 mysql_select_db ("750") ;
 */
 // ---------------- NNP ------------------------
 
-$link1 = mysql_connect ("localhost", "felipe", "KWMEEGvaIpiyIrsd")
+$link1 = mysql_connect ("localhost", "", "")
         or die ("No se puede conectar a MySQL ");
 
-mysql_select_db ("felipe") ;
+mysql_select_db ("") ;
 
 //
 
-$link2 = mysql_connect ("localhost", "felipe", "KWMEEGvaIpiyIrsd")
+$link2 = mysql_connect ("localhost", "", "")
         or die ("No se puede conectar a MySQL ");
 
-mysql_select_db ("felipe") ;
+mysql_select_db ("") ;
 
 ?>
